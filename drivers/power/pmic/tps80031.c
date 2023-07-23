6628be5ec8333c6eaead022e17e49b4aad891fef
Svyatoslav Ryhel <clamor95@gmail.com>
2023-07-23 19:58:56 +0300

    sysreset: implement TPS80031 sysreset functions
    TPS80031/TPS80032 PMICs have embedded power control functions
    used by some device to initiane device power off. Implement it as
    sysreset driver.
    
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- drivers/power/pmic/tps80031.c
+++ drivers/power/pmic/tps80031.c
@@ -9,6 +9,7 @@
 #include <dm.h>
 #include <i2c.h>
 #include <log.h>
+#include <dm/lists.h>
 #include <power/pmic.h>
 #include <power/regulator.h>
 #include <power/tps80031.h>
@@ -49,7 +50,16 @@ static int tps80031_read(struct udevice *dev, uint reg, uint8_t *buff, int len)
 static int tps80031_bind(struct udevice *dev)
 {
 	ofnode regulators_node;
-	int children;
+	int children, ret;
+
+	if (IS_ENABLED(CONFIG_SYSRESET_TPS80031)) {
+		ret = device_bind_driver(dev, TPS80031_RST_DRIVER,
+					 "sysreset", NULL);
+		if (ret) {
+			log_err("cannot bind SYSRESET (ret = %d)\n", ret);
+			return ret;
+		}
+	}
 
 	regulators_node = dev_read_subnode(dev, "regulators");
 	if (!ofnode_valid(regulators_node)) {
